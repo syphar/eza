aea63210f870791c2e252f6280146fdfaa32b744
Denis Cornehl <denis@cornehl.org>
2023-08-27 19:11:19 +0200

    upgrade term_grid, terminal_size, timeago

--- src/output/grid_details.rs
+++ src/output/grid_details.rs
@@ -278,6 +278,7 @@ impl<'a> Render<'a> {
                         let cell = grid::Cell {
                             contents: ANSIStrings(&column[row].contents).to_string(),
                             width:    *column[row].width,
+                            alignment: grid::Alignment::Left,
                         };
 
                         grid.add(cell);
@@ -291,6 +292,7 @@ impl<'a> Render<'a> {
                     let cell = grid::Cell {
                         contents: ANSIStrings(&cell.contents).to_string(),
                         width:    *cell.width,
+                        alignment: grid::Alignment::Left,
                     };
 
                     grid.add(cell);
